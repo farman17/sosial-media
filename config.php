d67aa0ab6d987ac4a01732f0ae5cfe52144269f8
farman17 <69154746+farman17@users.noreply.github.com>
2020-11-10 07:54:27 +0700

    Add files via upload

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 
-$db_host = "localhost";
-$db_user = "devopscilsy";
-$db_pass = "1234567890";
-$db_name = "dbsosmed";
+$db_host = getenv('DB_HOST');
+$db_user = getenv('DB_USER');
+$db_pass = getenv('DB_PASS');
+$db_name = 'dbpesbuk';
 
-try {    
-    //create PDO connection 
+try {
+    //create PDO connection
     $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
 } catch(PDOException $e) {
     //show error
