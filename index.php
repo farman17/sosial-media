6ea7804c16c98400526181a1fbacf553dc3cfe6b
farman17 <69154746+farman17@users.noreply.github.com>
2020-11-10 19:37:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
                 <div class="row">
                     <div class="col-md-8">
                         <h1>Pesbuk DevOps Cilsy</h1>
-                        <p>Bergabunglah bersama devops engineer di seluruh Indonesia</p>
+                        <p>Bergabunglah bersama devops engineer di seluruh Dunia</p>
                     </div>
                     <div class="col-md-4">
                         <a href="login.php" class="btn btn-secondary">Masuk</a>
